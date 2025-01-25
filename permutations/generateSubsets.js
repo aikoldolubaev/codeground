ebd7dfd9d123ed05eb8dbc9c1a52025371b93106
Aikol <imaikol@yahoo.com>
2025-01-25 00:42:08 -0800

    Update generateSubsets.js

--- permutations/generateSubsets.js
+++ permutations/generateSubsets.js
@@ -1,3 +1,4 @@
+// Backtracking with Iteration
 function generateSubsets(nums) {
 	const subsets = [] // To store all subsets
 
@@ -23,3 +24,32 @@ const output1 = [[], [1]]
 
 console.log(generateSubsets([1, 2, 3]))
 const output2 = [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+
+// Easier solution to understand recursion (Direct Recursion with Choices)
+function generateSubsets(nums) {
+	const totalNumbers = nums.length
+	const subsets = []
+	const currentSubset = []
+
+	function backtrack(index) {
+		// Base case
+		if (index === totalNumbers) {
+			subsets.push([...currentSubset])
+			return
+		}
+
+		// Exclude nums[index]
+		backtrack(index + 1)
+
+		// Include nums[index]
+		currentSubset.push(nums[index])
+		backtrack(index + 1)
+		currentSubset.pop()
+	}
+
+	backtrack(0)
+	return subsets
+}
+
+console.log(generateSubsets([1, 2]))
+const output3 = [[], [1, 2], [2], [1]]

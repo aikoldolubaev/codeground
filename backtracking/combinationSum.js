c0ca1c34e54951754c49a03589d41da5380c991e
Aikol <imaikol@yahoo.com>
2025-02-03 02:09:05 -0800

    Create combinationSum.js

---
+++ backtracking/combinationSum.js
@@ -0,0 +1,22 @@
+function combinationSum(candidates, target) {
+	let result = []
+
+	function backtrack(index, path, sum) {
+		if (sum === target) {
+			result.push([...path])
+			return
+		}
+		if (sum > target) return
+
+		for (let i = index; i < candidates.length; i++) {
+			path.push(candidates[i])
+			backtrack(i, path, sum + candidates[i]) // reuse same number
+			path.pop()
+		}
+	}
+
+	backtrack(0, [], 0)
+	return result
+}
+
+console.log(combinationSum([2, 3, 6, 7], 7)) // [[2,2,3], [7]]

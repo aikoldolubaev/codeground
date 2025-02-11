cb2945fb6002004e13d9c0fa78cc91bc5756e9f8
Aikol <imaikol@yahoo.com>
2025-02-10 16:21:11 -0800

    Create anagramGenerations.js

---
+++ backtracking/permutations/anagramGenerations.js
@@ -0,0 +1,18 @@
+function generateAnagrams(str) {
+  if (str.length <= 1) return [str];
+
+  let anagrams = [];
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    let remainingStr = str.slice(0, i) + str.slice(i + 1);
+    let remainingAnagrams = generateAnagrams(remainingStr);
+
+    for (let anagram of remainingAnagrams) {
+      anagrams.push(char + anagram);
+    }
+  }
+  return [...new Set(anagrams)]; // Remove duplicates
+}
+
+// Example usage:
+console.log(generateAnagrams("abc"));

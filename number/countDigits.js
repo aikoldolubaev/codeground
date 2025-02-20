518b832e634811717d680500e8b5ad2636211d7f
imaikol@yahoo.com <imaikol@yahoo.com>
2025-02-19 23:46:10 -0800

    Edit count digits

--- number/countDigits.js
+++ number/countDigits.js
@@ -1,14 +1,14 @@
-//find how many numbers in gives numbers
-//input=> 125; output=> 3
+// find how many numbers in a given number
+// Innput; 125; Output: 3
 function count(num) {
-	var count = 0;
+	var count = 0
 	if (!num) {
-		return 1;
+		return 1
 	}
 	while (num >= 1) {
-		num /= 10;
-		count++;
+		num /= 10
+		count++
 	}
-	return count;
+	return count
 }
-count(125);
+count(125)

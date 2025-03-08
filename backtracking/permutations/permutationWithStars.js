08ae710fc3a4b9421dabb5338bfd00921decc07f
Aikol <imaikol@yahoo.com>
2025-03-07 16:06:50 -0800

    Update permutationWithStars.js

--- backtracking/permutations/permutationWithStars.js
+++ backtracking/permutations/permutationWithStars.js
@@ -39,3 +39,30 @@ console.log(getPermutations('a*b*', 'cd')) // ["adbc", "adbd", "acbc", "acbd"]
 
 
 */
+
+function iterativePermutations(str, s) {
+	const result = [],
+		hash = {},
+		arr = [str]
+
+	while (arr.length) {
+		let curr = arr.shift().split('') // [ 'a', '*', 'b', '*' ]
+		for (let i = 0; i < curr.length; i++) {
+			if (curr[i] == '*') {
+				for (let j = 0; j < s.length; j++) {
+					curr[i] = s[j] //replacing "*" to "c"
+					let c = curr.join('') //turning back to string;  [ 'a', 'c', 'b', '*' ]  => "acb*"
+					if (c.includes('*')) {
+						arr.push(c) //"acb*"
+					} else if (!hash[c]) {
+						//making sure we don't insert duplicates
+						result.push(c)
+						hash[c] = true
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+iterativePermutations('a*b*', 'cd') //["adbc", "adbd", "acbc", "acbd"]

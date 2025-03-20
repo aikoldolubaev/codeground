d2d8754f94441e185336aa29efd17272a96fe972
aikoldolubaev <imaikol@yahoo.com>
2025-03-19 23:47:16 -0700

    Delete files

--- higher order functions/forEach.js
+++
@@ -1,6 +0,0 @@
-var arr = [1,3,4,5];
-
-arr.forEach((i, idx) => {
-    arr[idx]=arr[idx]+1
-});
-arr;
--- higher order functions/reduce.js
+++
@@ -1,8 +0,0 @@
-// REDUCE
-var ar = [1, 2, 3, 4];
-function reducer(accumulator, current, idx){
-//     return accumulator[current] = idx;//3
-    accumulator[current] = idx;//3
-    return accumulator;
-}
-ar.reduce(reducer, {});// {1: 0, 2: 1, 3: 2, 4: 3}

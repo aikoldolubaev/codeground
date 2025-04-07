ff0065f3671ac566e5ce78fd9cf31674b739806b
aikoldolubaev <imaikol@yahoo.com>
2025-04-07 00:24:09 -0700

    Update find difference

--- leetcode(easy)/findDiffs.js
+++ leetcode(easy)/findDiffs.js
@@ -1,40 +1,20 @@
-var findTheDifference = function(s, t) {
-    let sArr = s.split("")
-    let result = ''
-    let hash = {}
-    for(let i = 0; i < sArr.length; i++){
-        let letter = sArr[i]
-        hash[letter] = hash[letter]+1 || 1;
-    }
-    
-    let tArr = t.split("")
-    for(let j = 0; j < tArr.length; j++){
-        let letter = tArr[j];
+const findDifference = (input1, input2) => {
+	let difference = ''
+	let hash = {}
+	for (let i = 0; i < input1.length; i++) {
+		const letter = input1[i]
+		hash[letter] = hash[letter] + 1 || 1
+	}
 
-        if(!hash[letter] ||( hash[letter] && hash[letter] === 0)) {//if there is no left or do not exist
-            result += letter//add to the difference
-        }
-        hash[letter] = hash[letter] - 1//keep subtracking from s hash;
-        
-    }
-    return result;
-    
-};
+	for (let j = 0; j < input2.length; j++) {
+		const letter = input2[j]
 
+		if (!hash[letter]) {
+			difference += letter
+		}
+		hash[letter] = hash[letter] - 1 //keep subtracking from s hash;
+	}
+	return result
+}
 
-/* 
-Input:
-s = "abcd"
-t = "abcde"
-
-Output:
-e
-
-Explanation:
-'e' is the letter that was added.
-
-high level:
-    build a hashmap on first argument and on a second argument start decrementing from hashmap,
-     and if you find that value is zero or do not exist then add that into result and return result;
-
-*/
+console.log(findDifference('abcd', 'abcde')) // e

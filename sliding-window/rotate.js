ba7fcda141c2d8b9795301d1a6e97a27146d612c
aikoldolubaev <imaikol@yahoo.com>
2025-04-14 01:03:18 -0700

    Update

---
+++ sliding-window/rotate.js
@@ -0,0 +1,14 @@
+const rotation = (str1, str2) => {
+	if (str1.length !== str2.length) {
+		return false
+	}
+	for (let i = 0; i < str1.length; i++) {
+		const rotate = str1.slice(i, str1.length) + str1.slice(0, i)
+		if (rotate === str2) {
+			return true
+		}
+	}
+
+	return false
+}
+rotation('vaja', 'java') //true

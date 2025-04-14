a7c5ae3bf5f8989c7d22953459e914110be77631
aikoldolubaev <imaikol@yahoo.com>
2025-04-13 23:59:13 -0700

    Update files

--- array/mergingArrays.js
+++ array/mergingArrays.js
@@ -1,19 +1,19 @@
 //Merge 2 arrays in sorted order
-const mergeArrays = (a1, a2)=>{
-    const a = a1.concat(a2);
-    let sorted = true;
-    while(sorted){
-      sorted = false;
-      for(let i=0; i<a.length; i++){
-        if(a[i- 1] > a[i]){
-          sorted = true;
-          let temp = a[i-1];//swapping
-          a[i-1]= a[i];
-          a[i] = temp
-        }
-      }
-    }
-    return a;
-  }
-  
-  mergeArrays([3,2,1],[4,5,6]);
+const mergeArrays = (a1, a2) => {
+	const a = a1.concat(a2)
+	let sorted = true
+	while (sorted) {
+		sorted = false
+		for (let i = 0; i < a.length; i++) {
+			if (a[i - 1] > a[i]) {
+				sorted = true
+				let temp = a[i - 1] //swapping
+				a[i - 1] = a[i]
+				a[i] = temp
+			}
+		}
+	}
+	return a
+}
+
+mergeArrays([3, 2, 1], [4, 5, 6])

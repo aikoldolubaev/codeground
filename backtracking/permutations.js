b2784c71848611622b1766542733aaf8c683fe9c
Aikol <imaikol@yahoo.com>
2025-07-09 23:37:30 -0700

    Add comments to clarify iterative permutation logic and enhance example output

--- backtracking/permutations.js
+++ backtracking/permutations.js
@@ -35,6 +35,7 @@ const permuteIterative = (arr) => {
 
 		for (const perm of permutations) {
 			for (let i = 0; i <= perm.length; i++) {
+				// Insert `num` at every possible position in `perm`
 				const newPerm = [...perm.slice(0, i), num, ...perm.slice(i)]
 				nextPermutations.push(newPerm)
 			}
@@ -48,6 +49,24 @@ const permuteIterative = (arr) => {
 
 // Example usage:
 const result = JSON.stringify(permuteIterative([1, 2, 3]))
-console.log(result) // [[3,2,1],[2,3,1],[2,1,3],[3,1,2],[1,3,2],[1,2,3]]
+console.log(result)
+/* 
+Num is 3 and insert it in every possible position
+    [
+        [3,2,1],
+        [2,3,1],
+        [2,1,3],
+
+        [3,1,2],
+        [1,3,2],
+        [1,2,3]
+    ] 
+*/
 const result2 = JSON.stringify(permuteIterative([1, 2]))
-console.log(result2) //[[2,1],[1,2]]
+console.log(result2)
+/*
+    [
+        [2,1],
+        [1,2]
+    ]
+*/

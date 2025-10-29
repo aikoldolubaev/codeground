966b175350c020189a614dbd8b926a11a974d9df
Aikol Tashbolot <imaikol@yahoo.com>
2025-10-29 00:08:11 -0700

    bfs

--- bfs/rotting-oranges/index.js
+++ bfs/rotting-oranges/index.js
@@ -2,39 +2,38 @@
 // https://leetcode.com/problems/rotting-oranges/description/
 
 function orangesRotting(grid) {
-	const queue = [] // elements: [r, c, minute]
+	const queue = []
 	let fresh = 0
 	let minutes = 0
 
-	// init: collect rotten, count fresh
-	for (let r = 0; r < grid.length; r++) {
+	// Initialize the queue with all rotten oranges and count fresh ones
+	for (let row = 0; row < grid.length; row++) {
 		for (let c = 0; c < grid[0].length; c++) {
-			if (grid[r][c] === 2) queue.push([r, c, 0])
-			else if (grid[r][c] === 1) fresh++
+			if (grid[row][c] === 2) queue.push([row, c, 0])
+			else if (grid[row][c] === 1) fresh++
 		}
 	}
 	if (fresh === 0) return 0 // nothing to rot
 
-	// directions: down, up, right, left
-	const dirs = [
-		[1, 0],
-		[-1, 0],
-		[0, 1],
-		[0, -1],
+	const directions = [
+		[1, 0], // down
+		[-1, 0], // up
+		[0, 1], // right
+		[0, -1], // left
 	]
 
 	// BFS
 	while (queue.length) {
-		const [r, c, t] = queue.shift()
-		for (const [dr, dc] of dirs) {
-			const nr = r + dr,
-				nc = c + dc
-			if (nr < 0 || nr >= grid.length || nc < 0 || nc >= grid[0].length) continue
-			if (grid[nr][nc] !== 1) continue // not fresh
-			grid[nr][nc] = 2 // now rotten
+		const [row, col, time] = queue.shift()
+		for (const [dirRow, dirCol] of directions) {
+			const newRow = row + dirRow
+			const newCol = col + dirCol
+			if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length) continue
+			if (grid[newRow][newCol] !== 1) continue // not fresh
+			grid[newRow][newCol] = 2 // now rotten
 			fresh--
-			minutes = t + 1 // track last time used
-			queue.push([nr, nc, minutes])
+			minutes = time + 1 // track last time used
+			queue.push([newRow, newCol, minutes])
 		}
 	}
 

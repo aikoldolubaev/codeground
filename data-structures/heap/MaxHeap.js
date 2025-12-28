e4fadd26a1e935d647cc91898cace6e6f7c0008f
Aikol Tashbolot <imaikol@yahoo.com>
2025-12-28 01:58:16 -0800

    Refactor heapify methods for clarity and efficiency in MaxHeap.js

--- data-structures/heap/MaxHeap.js
+++ data-structures/heap/MaxHeap.js
@@ -14,7 +14,7 @@ class MaxHeap {
 		return 2 * i + 2
 	}
 	swap(i, j) {
-		[this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]]
+		;[this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]]
 	}
 
 	insert(value) {
@@ -23,12 +23,35 @@ class MaxHeap {
 	}
 
 	heapifyUp() {
-		let index = this.heap.length - 1
-		while (index > 0) {
-			let parentIndex = this.getParentIndex(index)
-			if (this.heap[parentIndex] >= this.heap[index]) break
-			this.swap(parentIndex, index)
-			index = parentIndex
+		let lastIndex = this.heap.length - 1
+		// While not at the root and parent is less than current
+		while (lastIndex > 0) {
+			let parentIndex = this.getParentIndex(lastIndex)
+			// If parent is greater or equal than last index, we're done
+			if (this.heap[parentIndex] >= this.heap[lastIndex]) break
+			this.swap(parentIndex, lastIndex)
+			// Move up to parent index
+			lastIndex = parentIndex
+		}
+	}
+
+	heapifyDown() {
+		let index = 0
+		const hasLeftChild = this.getLeftChildIndex(index) < this.heap.length
+		while (hasLeftChild) {
+			let leftChildIndex = this.getLeftChildIndex(index)
+			let rightChildIndex = this.getRightChildIndex(index)
+
+			const hasRightChild = this.getRightChildIndex(index) < this.heap.length
+			const isRightChildBigger = this.heap[rightChildIndex] > this.heap[leftChildIndex]
+			if (hasRightChild && isRightChildBigger) {
+				leftChildIndex = rightChildIndex
+			}
+
+			// If current is greater or equal than left child
+			if (this.heap[index] >= this.heap[leftChildIndex]) break
+			this.swap(index, leftChildIndex)
+			index = leftChildIndex
 		}
 	}
 
@@ -41,22 +64,6 @@ class MaxHeap {
 		this.heap[0] = this.heap.pop()
 		this.heapifyDown()
 		return max
-	}
-
-	heapifyDown() {
-		let index = 0
-		while (this.getLeftChildIndex(index) < this.heap.length) {
-			let largerChildIndex = this.getLeftChildIndex(index)
-			let rightChildIndex = this.getRightChildIndex(index)
-
-			if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] > this.heap[largerChildIndex]) {
-				largerChildIndex = rightChildIndex
-			}
-
-			if (this.heap[index] >= this.heap[largerChildIndex]) break
-			this.swap(index, largerChildIndex)
-			index = largerChildIndex
-		}
 	}
 
 	print() {

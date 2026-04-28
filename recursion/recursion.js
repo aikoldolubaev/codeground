638baeb7237291ef092efb744a1e5842fa7417bc
Aikol Tashbolot <imaikol@yahoo.com>
2026-04-27 23:41:40 -0700

    Update

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,16 +1,17 @@
 const recursion = (num) => {
 	if (num === 0) return 0
-	console.log('✋ Before:', num)
+	console.log('📚 Before:', num)
 	recursion(num - 1)
-	console.log('✅ After:', num)
+	console.log('📭 After:', num)
+
 	return num // if not return num, it will return undefined
 }
 
 console.log(recursion(3))
-// ✋ Before: 3
-// ✋ Before: 2
-// ✋ Before: 1
-// ✅ After: 1
-// ✅ After: 2
-// ✅ After: 3
+// 📚 Before: 3
+// 📚 Before: 2
+// 📚 Before: 1
+// 📭 After: 1
+// 📭 After: 2
+// 📭 After: 3
 // 3

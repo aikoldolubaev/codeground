85d634a6ec2de1aa0f0fc20f820d3dd71f13ded3
Aikol Tashbolot <imaikol@yahoo.com>
2026-04-29 00:29:05 -0700

    Update var

--- backtracking/subsets/subsets.js
+++ backtracking/subsets/subsets.js
@@ -1,28 +1,27 @@
 function generateSubsets(options) {
 	const subsets = []
-	const stack = []
+	const path = []
 
 	function backtrack(index) {
 		// Base case
 		if (index === options.length) {
-			subsets.push([...stack])
+			subsets.push([...path])
 			return
 		}
 
 		// As is
 		backtrack(index + 1)
 
-		// Include nums[index]
-
-		stack.push(options[index])
+		// Include
+		path.push(options[index])
 		backtrack(index + 1)
 
-		stack.pop() // Undo the choice (backtrack)
+		path.pop() // Undo the choice
 	}
 
 	backtrack(0)
 	return subsets
 }
 
-// console.log(generateSubsets([1])) // [[], [1]]
-console.log(JSON.stringify(generateSubsets([1, 2]))) // [[], [1], [2], [1, 2]]
+console.log(generateSubsets([1])) // [[], [1]]
+// console.log(JSON.stringify(generateSubsets([1, 2]))) // [[], [1], [2], [1, 2]]
